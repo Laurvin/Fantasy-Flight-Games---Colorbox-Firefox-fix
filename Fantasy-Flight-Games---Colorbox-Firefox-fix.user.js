67c82cb71e8d5b0edfa8033a9d873233dab88a7c
Laurvin <github@dbbv.com>
2018-10-06 14:33:16 +0200

    Update Fantasy-Flight-Games---Colorbox-Firefox-fix.user.js

--- Fantasy-Flight-Games---Colorbox-Firefox-fix.user.js
+++ Fantasy-Flight-Games---Colorbox-Firefox-fix.user.js
@@ -3,10 +3,11 @@
 // @namespace Fantasy Flight Games - Colorbox Firefox fix
 // @author Laurvin
 // @description The Colorbox script does not remove the scrollbars on Firefox by default, covering up part of the images; this fixes that.
-// @version 0.1
+// @version 0.2
 // @icon http://i.imgur.com/XYzKXzK.png
 // @downloadURL https://github.com/Laurvin/Fantasy-Flight-Games---Colorbox-Firefox-fix/raw/master/Fantasy-Flight-Games---Colorbox-Firefox-fix.user.js
 // @include https://www.fantasyflightgames.com/en/news/*
+// @include https://drafts.fantasyflightgames.com/en/news/*
 // @grant none
 // @run-at document-idle
 // ==/UserScript==
